ead136469231c654fc2149f894aeb212c8b1b331
Slavi Pantaleev <slavi@devture.com>
2022-12-24 17:30:29 +0200

    Fix deprecations

--- Twig/TranslationExtension.php
+++ Twig/TranslationExtension.php
@@ -3,20 +3,17 @@ namespace Devture\Bundle\TranslationBundle\Twig;
 
 class TranslationExtension extends \Twig\Extension\AbstractExtension {
 
-	private $locales;
-
-	public function __construct(array $locales) {
-		$this->locales = $locales;
+	public function __construct(private array $locales) {
 	}
 
-	public function getName() {
+	public function getName(): string {
 		return 'devture_translation_extension';
 	}
 
-	public function getFunctions() {
-		return array(
+	public function getFunctions(): array {
+		return [
 			new \Twig\TwigFunction('devture_translation_get_locale_name', [$this, 'getLocaleName']),
-		);
+		];
 	}
 
 	public function getLocaleName(string $localeKey): ?string {
